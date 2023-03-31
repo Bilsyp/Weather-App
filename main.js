7a1ee2b174599330d3693cd6cecd6e8b5c40ecc5
Bilsyp <bilalsyah77@gmail.com>
2023-03-31 17:45:08 +0700

    issue-3

--- main.js
+++ main.js
@@ -7,17 +7,10 @@ const ios = _.max([4, 2, 8, 6]);
 const start = _.debounce(search, [600])
 
 async function fetchData(lat, lon) {
-  if (input.value !== "") {
     const fetchDatas = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${input.value}`)
     const data = await fetchDatas.json()
     return data
-  } else {
-    // with Parameter for Default
-    const fetchDatax = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${lat},${lon}`)
-    const datax = await fetchDatax.json()
-    return datax
-  }
-
+  
 }
 async function search() {
   const ul = document.querySelector("form ul")
